cb91df97526755d20a5b91a574cf168399ad5fc9
Wlidemberg <automatizei@gmail.com>
2026-03-31 15:05:03 -0300

    refactor: inclusão do selectorList → lista de seletores

--- aula/primeiros-passos-cypress/cypress/e2e/login_spec.cy.js
+++ aula/primeiros-passos-cypress/cypress/e2e/login_spec.cy.js
@@ -1,33 +1,45 @@
 describe('Orage HRM Testes', () => {
+
+  const selectorList = {
+    username: '[name="username"]',
+    password: '[name="password"]',
+    loginButton: '.oxd-button',
+    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb > .oxd-text',
+    alertInputMessage: '.oxd-input-group__message',
+    alertInvalidCredentials: '.oxd-alert-content',
+
+
+  }
+
   it('Login com sucesso', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('Admin')
-    cy.get('[name="password"]').type('admin123')
-    cy.get('.oxd-button').click()
+    cy.get(selectorList.username).type('Admin')
+    cy.get(selectorList.password).type('admin123')
+    cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('eq', '/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
   })
 
   it('Login sem credenciais', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('.oxd-button').click()
-    cy.get('.oxd-input-group__message').should('have.length', 2)
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.alertInputMessage).should('have.length', 2)
   })
 
   it('Login com usuario incorrento', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('AdminTeste')
-    cy.get('[name="password"]').type('admin123')
-    cy.get('.oxd-button').click()
-    cy.get('.oxd-alert-content').should('contain', 'Invalid credentials')
+    cy.get(selectorList.username).type('AdminTeste')
+    cy.get(selectorList.password).type('admin123')
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.alertInvalidCredentials).should('contain', 'Invalid credentials')
 
   })
 
   it('Login com senha incorreta', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('Admin')
-    cy.get('[name="password"]').type('admin123Teste')
-    cy.get('.oxd-button').click()
-    cy.get('.oxd-alert-content').should('contain', 'Invalid credentials')
+    cy.get(selectorList.username).type('Admin')
+    cy.get(selectorList.password).type('admin123Teste')
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.alertInvalidCredentials).should('contain', 'Invalid credentials')
   })
 })
