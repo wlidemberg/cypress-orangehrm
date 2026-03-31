c88c44f48f863a84c3feee19ba688b475b27d670
Wlidemberg <automatizei@gmail.com>
2026-03-31 16:11:49 -0300

    chore: definição baseUrl em cypress.config.js

--- aula/primeiros-passos-cypress/cypress/e2e/login_spec.cy.js
+++ aula/primeiros-passos-cypress/cypress/e2e/login_spec.cy.js
@@ -13,7 +13,7 @@ describe('Orage HRM Testes', () => {
 
 
   it('Login com sucesso', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.username).type(userData.userSuccess.username)
     cy.get(selectorList.password).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
@@ -22,13 +22,13 @@ describe('Orage HRM Testes', () => {
   })
 
   it('Login sem credenciais', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.loginButton).click()
     cy.get(selectorList.alertInputMessage).should('have.length', 2)
   })
 
   it('Login com usuario incorrento', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.username).type(userData.userNameFail.username)
     cy.get(selectorList.password).type(userData.userNameFail.password)
     cy.get(selectorList.loginButton).click()
@@ -37,7 +37,7 @@ describe('Orage HRM Testes', () => {
   })
 
   it('Login com senha incorreta', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.username).type(userData.userNameFail.username)
     cy.get(selectorList.password).type(userData.userNameFail.password)
     cy.get(selectorList.loginButton).click()
