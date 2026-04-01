4a79ad3a4bca5e3ae9f70a3bf2bf63d599570517
Wlidemberg <automatizei@gmail.com>
2026-04-01 11:34:22 -0300

    refactor(test): Ajuste user_spec

--- aula/primeiros-passos-cypress/cypress/e2e/user_spec.cy.js
+++ aula/primeiros-passos-cypress/cypress/e2e/user_spec.cy.js
@@ -9,16 +9,36 @@ describe('Orage HRM Testes', () => {
     sectionTitleTopBar: '.oxd-topbar-header-breadcrumb > .oxd-text',
     alertInputMessage: '.oxd-input-group__message',
     alertInvalidCredentials: '.oxd-alert-content',
+    buttonDetails: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstName: '[name="firstName"]',
+    middleName: '[name="middleName"]',
+    lastName: '[name="lastName"]',
   }
 
 
-  it('Login com sucesso', () => {
+  it.only('Login com sucesso', () => {
     cy.visit('/auth/login')
     cy.get(selectorList.username).type(userData.userSuccess.username)
     cy.get(selectorList.password).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('eq', '/web/index.php/dashboard/index')
     cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorList.buttonDetails).click()
+
+    cy.contains('label', 'Employee Id').parent().next().find('input').clear().type('333456')
+    cy.contains('label', 'Other Id').parent().next().find('input').clear().type('654321')
+    cy.contains('label', "Driver's License Number").parent().next().find('input').clear().type('abc123')
+    cy.contains('label', 'Nationality').parent().next().find('.oxd-select-text-input').click()
+    cy.contains('.oxd-select-option', 'Brazilian').click()
+    cy.contains('label', 'Marital Status').parent().next().find('.oxd-select-text-input').click()
+    cy.contains('.oxd-select-option', 'Single').click()
+
+    cy.contains('label', 'Date of Birth').parent().next().find('input').type('1980-15-06')
+    cy.get('.--close').click()
+
+    cy.get('.oxd-button').eq(1).click()
+
+    cy.get('.oxd-toast').should('be.visible').and('contain', 'Success')
   })
 
   it('Login sem credenciais', () => {
