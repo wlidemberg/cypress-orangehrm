73ff9375af4008e17832356d26dcb832c4883913
Wlidemberg <automatizei@gmail.com>
2026-04-15 13:05:11 -0300

    refactor(test): Ajuste user_spec

--- aula/primeiros-passos-cypress/cypress/e2e/user_spec.cy.js
+++ aula/primeiros-passos-cypress/cypress/e2e/user_spec.cy.js
@@ -1,44 +1,63 @@
 import userData from '../fixtures/user/userData.json'
+import LoginPages from '../Pages/LoginPages'
+import DashboardPages from '../Pages/DashboardPages'
 
+// Criando uma instância da classe LoginPages para usar seus métodos
+const loginPage = new LoginPages() 
+const dashboardPage = new DashboardPages()
 describe('Orage HRM Testes', () => {
 
   const selectorList = {
-    username: '[name="username"]',
-    password: '[name="password"]',
-    loginButton: '.oxd-button',
-    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb > .oxd-text',
+   
+    //sectionTitleTopBar: '.oxd-topbar-header-breadcrumb > .oxd-text',
     alertInputMessage: '.oxd-input-group__message',
-    alertInvalidCredentials: '.oxd-alert-content',
     buttonDetails: '[href="/web/index.php/pim/viewMyDetails"]',
     firstName: '[name="firstName"]',
     middleName: '[name="middleName"]',
     lastName: '[name="lastName"]',
+    genericField: '.oxd-input--active'
   }
 
 
   it.only('Login com sucesso', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.username).type(userData.userSuccess.username)
-    cy.get(selectorList.password).type(userData.userSuccess.password)
-    cy.get(selectorList.loginButton).click()
-    cy.location('pathname').should('eq', '/web/index.php/dashboard/index')
-    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
+
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+    
+    dashboardPage.checkDashboardPage()
+
     cy.get(selectorList.buttonDetails).click()
-
-    cy.contains('label', 'Employee Id').parent().next().find('input').clear().type('333456')
-    cy.contains('label', 'Other Id').parent().next().find('input').clear().type('654321')
-    cy.contains('label', "Driver's License Number").parent().next().find('input').clear().type('abc123')
-    cy.contains('label', 'Nationality').parent().next().find('.oxd-select-text-input').click()
-    cy.contains('.oxd-select-option', 'Brazilian').click()
-    cy.contains('label', 'Marital Status').parent().next().find('.oxd-select-text-input').click()
-    cy.contains('.oxd-select-option', 'Single').click()
-
-    cy.contains('label', 'Date of Birth').parent().next().find('input').type('1980-15-06')
+    cy.get(selectorList.firstName).clear().type('NameTest')
+    cy.get(selectorList.middleName).clear().type('MiddleNameTest')
+    cy.get(selectorList.lastName).clear().type('LastNameTest')
+    cy.get('.oxd-form-row').eq(1).find('input').eq(0).clear().type('employee')
+    cy.get('.oxd-form-row').eq(1).find('input').eq(1).clear().type('otherId')
+    cy.get('.oxd-form-row').eq(1).find('input').eq(2).clear().type('driverLicense')
+    cy.get('.oxd-form-row').eq(1).find('input').eq(3).clear().type('2038-15-06')
     cy.get('.--close').click()
-
+    cy.get('.oxd-form-row').eq(2).find('.oxd-select-text-input').eq(0).click()
+    cy.contains('.oxd-select-option', 'Afghan').click()
+    cy.get('.oxd-form-row').eq(2).find('.oxd-select-text-input').eq(1).click()
+    cy.contains('.oxd-select-option', 'Single').click()
+    cy.get('.oxd-form-row').eq(2).find('input').eq(0).clear().type('1980-06-15')
+    cy.get('.--close').click()
+    cy.get('.oxd-form-row').eq(2).find('.oxd-radio-wrapper').eq(1).click()
     cy.get('.oxd-button').eq(1).click()
 
     cy.get('.oxd-toast').should('be.visible').and('contain', 'Success')
+
+    cy.get('.oxd-form-row').eq(3).find('.oxd-select-text-input').eq(0).click()
+    cy.contains('.oxd-select-option', 'A-').click()
+    cy.get('.oxd-form-row').eq(3).find('input').clear().type('Comment Test other')
+    cy.get('.oxd-form-actions').eq(1).find('.oxd-button').click()
+    cy.get('.oxd-toast').should('be.visible').and('contain', 'Success')
+
+    cy.get('.orangehrm-action-header').find('.oxd-button').click()
+    cy.get('.oxd-file-button').click()
+    cy.get('input[type="file"]').selectFile('cypress/fixtures/files/test.png', {force: true})
+    cy.get('.oxd-file-input-div').should('contain', 'test.png')
+    cy.get('.oxd-form-row').eq(5).find('.oxd-textarea').clear().type('Arquivo anexado!')
+    cy.get('.oxd-button').eq(3).click()
   })
 
   it('Login sem credenciais', () => {
