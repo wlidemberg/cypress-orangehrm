66dc77cebb25cd86172c40c22abd7ba16389d216
Wlidemberg <automatizei@gmail.com>
2026-04-15 13:04:08 -0300

    feat: inclusão do LoginPages.js

---
+++ aula/primeiros-passos-cypress/cypress/Pages/LoginPages.js
@@ -0,0 +1,25 @@
+// Primeiro cria classe de login.
+class LoginPages {
+     // Cria o método para cada ação, como acessar a página, preencher campos e clicar
+     // No Page object colocamos a inteligencia [ACT](clicar, esperar, selecionar, etc)
+     selectorList(){
+        const selectors = {
+            usernameField: '[name="username"]',
+            passwordField: '[name="password"]',
+            loginButton: '[type="submit"]',
+            alertInvalidCredentials: '.oxd-alert-content',
+        }
+        return selectors
+     }
+
+     accessLoginPage(){
+        cy.visit('/auth/login')
+     }
+
+     loginWithUser(username, password){
+        cy.get(this.selectorList().usernameField).type(username)
+        cy.get(this.selectorList().passwordField).type(password)
+        cy.get(this.selectorList().loginButton).click()
+     }
+}
+export default LoginPages;
